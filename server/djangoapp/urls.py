2a469bcea4b5afd9cdb61f38f70a54e2866d1ab1
Wakwandou <williamkwandou@gmail.com>
2023-06-21 01:46:33 -0400

    Module 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,14 +14,14 @@ urlpatterns = [
     # path for contact us view
     path('contact/', views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for add a review view
 
